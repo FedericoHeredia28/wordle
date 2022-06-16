57bc20eb507938a06a93d152c51f4f2782c778c1
heredia <heredia>
2022-06-16 08:49:38 -0300

    entrego tarde pero autorizado

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // palabras para el juego, agregar las que quieran siempre de 5 letras
 // es un array de strings (dict por dictionary)
+const intentos = 6;
+let intentosRestantes = intentos;
+let intentoActual = "perro";
+let siguienteLetra = 0;
 let dict = [
   'gatos',
   'papas',
@@ -21,5 +25,23 @@ let dict = [
   'vuelo',
   'avion',
   'genio',
-  'pibes'
+  'pibes',
+  'manta',
+  'menta'
 ];
+
+let r = Math.random() * 21;
+r = Math.trunc(r);
+let correcto = dict[r]
+if (correcto == intentoActual) alert("se");
+let i = 0;
+for (let i = 0; i < 6; i++) {
+  let div = document.createElement('div');
+  div.textContent = i;
+  for (let c == of intentoActual) {
+    if (c == intentoActual) {
+      div.classList.add('a');
+    }
+  }
+  document.body.append(div);
+}
